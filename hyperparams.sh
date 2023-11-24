7e6dd204e781306e5d2e2337846f194946c2b185
marta <marta.knezevic10201@gmail.com>
2023-11-24 08:40:25 +0100

    fix script

--- hyperparams.sh
+++ hyperparams.sh
@@ -1,7 +1,7 @@
 dataset="swissprot"
 
 # feat
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         for head in 1 2 4; do
             for ff_dim in 512 1024 2048; do
@@ -18,7 +18,7 @@ for n_way in {3..8..2}; do
 done
 
 # fealstm
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         for num_layers in 1 2 3 4; do
             for score in EuclideanDistanceScore CosineSimilarityScore; do
@@ -31,28 +31,28 @@ for n_way in {3..8..2}; do
 done
 
 # baseline
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         python run.py exp.name=${dataset}_baseline method=baseline dataset=${dataset} n_way=$n_way n_shot=$n_shot method.stop_epoch=40
     done
 done
 
 # baseline_pp
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         python run.py exp.name=${dataset}_baseline_pp method=baseline_pp dataset=${dataset} n_way=$n_way n_shot=$n_shot method.stop_epoch=40
     done
 done
 
 # maml
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         python run.py exp.name=${dataset}_maml method=maml dataset=${dataset} n_way=$n_way n_shot=$n_shot method.stop_epoch=40
     done
 done
 
 # matchingnet
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         python run.py exp.name=${dataset}_matchingnet method=matchingnet dataset=${dataset} n_way=$n_way n_shot=$n_shot method.stop_epoch=40
     done
@@ -60,7 +60,7 @@ done
 
 
 # protonet
-for n_way in {3..8..2}; do
+for n_way in 3 5 7; do
     for n_shot in 1 5; do
         python run.py exp.name=${dataset}_protonet method=protonet dataset=${dataset} n_way=$n_way n_shot=$n_shot method.stop_epoch=40
     done
