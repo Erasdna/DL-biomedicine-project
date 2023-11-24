91cd364827665ea0a0e2aabcef97dff27bca014e
marta <marta.knezevic10201@gmail.com>
2023-11-24 08:44:36 +0100

    fix script

--- hyperparams.sh
+++ hyperparams.sh
@@ -8,7 +8,7 @@ for n_way in 3 5 7; do
                 for dropout in 0.2 0.1 0.05 0.01; do
                     for score in EuclideanDistanceScore CosineSimilarityScore; do
                         for lr in 0.001 0.0001 0.005; do
-                            python run.py exp.name=${dataset}_feat method=feat dataset=${dataset} n_way=$n_way n_shot=$n_shot method.cls.head=$head method.cls.dim_feedforward=$ff_dim method.cls.dropout=$dropout method.cls.score=methods.settoset.settoset.$score lr=$lr method.stop_epoch=40
+                            python run.py exp.name=${dataset}_feat method=feat dataset=${dataset} n_way=$n_way n_shot=$n_shot method.cls.n_head=$head method.cls.dim_feedforward=$ff_dim method.cls.dropout=$dropout method.cls.score=methods.settoset.settoset.$score lr=$lr method.stop_epoch=40
                         done
                     done
                 done
